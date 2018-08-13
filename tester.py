06893bf471c51b572fba66953561cad5106d80e4
Claus Krag <ckra@tv2.dk>
2018-08-13 14:18:12 +0200

    added log interval as script parameter

--- tester.py
+++ tester.py
@@ -2,14 +2,20 @@
 
 import log
 import time
+import sys
 
 _LOG_INTERVAL_SEC = 60
 
 # Run
+if len(sys.argv) > 1:
+    _LOG_INTERVAL_SEC = int(sys.argv[1])
+
 log = log.Log()
 
 print("Logging started")
+print("Log interval: {}".format(_LOG_INTERVAL_SEC))
 
 while True:
+    print(str(log._get_timestamp()) + " - testing ..")
     log.log()
     time.sleep(_LOG_INTERVAL_SEC)
